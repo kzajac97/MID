b175ad24636ebeff1e4483250d5103cbda50f1d6
Krzysztof <kzajac1997@gmail.com>
2020-03-03 20:24:51 +0100

    Update generators with explicit typing and variable names

--- mid/source/generators.py
+++ mid/source/generators.py
@@ -1,7 +1,10 @@
-from typing import Callable, Dict
+from typing import Any, Callable, Dict
 
 import numpy as np
 from scipy import signal
+
+FIRST_ITEM_INDEX = 0
+SECOND_ITEM_INDEX = 1
 
 
 def _sine_distribution(n_points: int, omega: float = 10 * np.pi) -> np.array:
@@ -14,9 +17,9 @@ def _sine_distribution(n_points: int, omega: float = 10 * np.pi) -> np.array:
     :return: numpy array with random distribution samples
     """
     distribution = np.zeros(n_points)
-    distribution[0] = np.random.rand()
+    distribution[FIRST_ITEM_INDEX] = np.random.rand()
     
-    for index in range(1, n_points):
+    for index in range(SECOND_ITEM_INDEX, n_points):
         distribution[index] = 0.5 * (np.sin(omega * distribution[index - 1]) + 1)
         
     return distribution
@@ -24,7 +27,7 @@ def _sine_distribution(n_points: int, omega: float = 10 * np.pi) -> np.array:
 
 def _sawtooth_distribution(n_points: int, period: float = 10 * np.pi) -> np.array:
     """
-    Generate random numbers from sawtotoh distribution 
+    Generate random numbers from sawtooth distribution
     
     :param n_points: number of generated data points
     :param period: sawtooth period
@@ -32,22 +35,22 @@ def _sawtooth_distribution(n_points: int, period: float = 10 * np.pi) -> np.arra
     :return: numpy array with random distribution samples
     """
     distribution = np.zeros(n_points)
-    distribution[0] = np.random.rand()
+    distribution[FIRST_ITEM_INDEX] = np.random.rand()
     
-    for index in range(1, n_points):
+    for index in range(SECOND_ITEM_INDEX, n_points):
         distribution[index] = signal.sawtooth(period * distribution[index - 1])
         
     return distribution
 
 
-def _fibonacci_distribution(n_points: int, p: int = 8, q: int = 4, m: int = 100) -> np.array:
+def _fibonacci_distribution(n_points: int, p: int = 8, q: int = 4, modulo_factor: int = 100) -> np.array:
     """
     Generate random number with fibonacci distribution
     
     :param n_points: number of generated data points
     :param p: fibonacci param, defaults to 8, must be greater than q
     :param q: fibonacci param, defaults to 4, must be less than p
-    :param m: modulo param, defaults to 100, should be in range <100, 10_000>
+    :param modulo_factor: modulo param, defaults to 100, should be in range <100, 10_000>
     
     :return: numpy array with random distribution samples
     """
@@ -55,14 +58,17 @@ def _fibonacci_distribution(n_points: int, p: int = 8, q: int = 4, m: int = 100)
     distribution[:p] = np.random.rand(p)
     
     for index in range(p, n_points):
-        distribution[index] = (distribution[index - p] + distribution[index - q]) % m
+        distribution[index] = (distribution[index - p] + distribution[index - q]) % modulo_factor
         
     return distribution
 
-distribution_name_mapping = {'uniform': np.random.rand,
-                             'sine': _sine_distribution,
-                             'sawtooth': _sawtooth_distribution,
-                             'fibonacci': _fibonacci_distribution}
+
+distribution_name_mapping: Dict[str, Callable[[Any], Any]] = {
+    'uniform': np.random.rand,
+    'sine': _sine_distribution,
+    'sawtooth': _sawtooth_distribution,
+    'fibonacci': _fibonacci_distribution}
+
 
 def random_distribution(n_points: int, distribution: str, **kwargs) -> np.array:
     """
