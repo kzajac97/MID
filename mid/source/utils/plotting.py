443de66bb6e376ec9c18726cd21b5af562526f5d
Krzysztof <kzajac1997@gmail.com>
2020-05-23 09:21:09 +0200

    Add notebook with least squares estimator analysis

---
+++ mid/source/utils/plotting.py
@@ -0,0 +1,14 @@
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def covariance_plot(covariance_matrix: np.array) -> None:
+    """
+    Utility for quick matrix heatmap plot with default parameters
+    :param covariance_matrix: square numpy matrix or array to display
+    """
+    plt.figure(figsize=[12, 12])
+    _ = sns.heatmap(covariance_matrix, annot=True, fmt=".2f", cbar=False, linewidth=0.1, cmap="coolwarm")
+
+    return
